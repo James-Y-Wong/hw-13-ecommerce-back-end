a61f82f6caefb5368832e2055e1cd517a13e10da
James-Y-Wong <jamesyeuwong@gmail.com>
2022-06-02 23:37:22 -0400

    modified some validations for product and product tag models

--- models/Product.js
+++ models/Product.js
@@ -21,10 +21,10 @@ Product.init(
       allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.FLOAT,
       allowNull: false,
       validate: {
-        isDecimal: true
+        isFloat: true
       }
     },
     stock: {
@@ -40,7 +40,6 @@ Product.init(
       references: {
         model: 'category',
         key: 'id',
-        unique: false
       }
     }
   },
